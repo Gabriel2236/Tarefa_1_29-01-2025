d3502b65c68666b8743820d0e7038583e2c049d1
Gabriel Oliveira <gab.costa2236@gmail.com>
2025-02-01 22:37:28 -0300

    Adicionado o temporizador com os leds

--- Temporizador.c
+++ Temporizador.c
@@ -1,13 +1,63 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
+#include "hardware/timer.h"
 
+#define g_led 11
+#define y_led 12
+#define r_led 13
 
-int main()
-{
+uint ledon = 0;
+
+bool repeating_timer_callback(struct repeating_timer *t) {
+    switch (ledon) {
+        case 0:
+           
+            gpio_put(r_led, false);
+
+            
+            gpio_put(g_led, true);
+
+            ledon++;
+            break;
+
+        case 1:
+           
+            gpio_put(g_led, false);
+            gpio_put(y_led, true);
+            ledon++;
+            
+            break;
+
+        case 2:
+            
+            gpio_put(y_led, false);
+            gpio_put(r_led, true);
+
+            ledon = 0;
+            break;
+    }
+    return true; // Adiciona retorno booleano
+}
+
+int main() {
     stdio_init_all();
+
+    gpio_init(g_led);
+    gpio_set_dir(g_led, GPIO_OUT);
+
+    gpio_init(y_led);
+    gpio_set_dir(y_led, GPIO_OUT);
+
+    gpio_init(r_led);
+    gpio_set_dir(r_led, GPIO_OUT);
+
+    struct repeating_timer temporizador;
+    add_repeating_timer_ms(3000, repeating_timer_callback, NULL, &temporizador);
 
     while (true) {
         printf("Hello, world!\n");
         sleep_ms(1000);
     }
+
+return 0;
 }
